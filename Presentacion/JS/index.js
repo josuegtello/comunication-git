8a42f71abf4867ae0537f502bed88350e582d54c
Gerardo Guzman <guzman.tello.gerardo.josue@gmail.com>
2024-02-25 12:02:51 -0600

    Funciones de jugadores hechas y manejo dinamico de los jugadores

--- Presentacion/JS/index.js
+++ Presentacion/JS/index.js
@@ -13,15 +13,52 @@ const dinamicIcons=[ //iconos dinamicos que se ocupar a lo largo del codigo
  `<svg xmlns="http://www.w3.org/2000/svg" height="38" viewBox="0 -960 960 960" width="38">
     <path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Z"/>
  </svg>`,//darkIcon
+ `<svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48">
+    <path d="m249 849-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z"/>
+ </svg>`,//close ICOn
 ];
 //CREAR OBJETO DONDE VA A IR 
-const p={
-    $section:null,
-    $caurosel:null,
-    $content:null,
-    actualPosition:null,
-}
+
 //VARIABLES NECESARIAS PARA APLICAR EL LIGHT Y EL DARK MODE
+
+//RETRASO EN MS en funciones asincronas
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+//Funcion para EMPEZAR Y MOSTRAR ANIMACIONES
+const animationPage=async function(Source,boolAtribute){
+    const $bar=document.querySelectorAll(".bar-animaton");
+    document.querySelector(".container-animation").style.setProperty("pointer-events","all");
+    source=Source;
+    const $button=document.querySelector(".button-container");
+    if(!$button.classList.contains("pointer-events-none")){
+        $button.style.setProperty("opacity","0");
+       await timeout(550);
+       $button.classList.add("pointer-events-none");
+    }
+    if(boolAtribute===0){
+        for (let i = 0; i < $bar.length; i++) {
+            $bar[i].style.setProperty("transform","translateX(100%)");
+            await timeout(200);
+        }
+        document.querySelector(".header-container").style.setProperty("transform","translateY(0%)");//bajamos el cursor
+        document.querySelector(".container-animation").style.setProperty("pointer-events","none");
+       
+    }
+    if(boolAtribute===1){
+        //document.querySelector(".section").style.setProperty("pointer-events","none");
+        for (let i = $bar.length-1;i>=0;i--) {
+            $bar[i].style.setProperty("transform","translateX(0%)");
+            await timeout(200);
+        }
+        if(source!==null){
+            console.log(source)
+            window.open(source,"_self","n");    
+        }
+    }     
+    return true;    
+}
+//Funcion para el modo claro y el modo oscuro
 const colorsVariables=[
     "--first-color",
     "--first-1-color",
@@ -118,44 +155,6 @@ const nameKey=[
     "linkColor",
     "textColor",
 ]
-//RETRASO EN MS en funciones asincronas
-function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-//Funcion para EMPEZAR Y MOSTRAR ANIMACIONES
-const animationPage=async function(Source,boolAtribute){
-    const $bar=document.querySelectorAll(".bar-animaton");
-    document.querySelector(".container-animation").style.setProperty("pointer-events","all");
-    source=Source;
-        const $button=document.querySelector(".button-container");
-    if(!$button.classList.contains("pointer-events-none")){
-        $button.style.setProperty("opacity","0");
-       await timeout(550);
-       $button.classList.add("pointer-events-none");
-    }
-    if(boolAtribute===0){
-        for (let i = 0; i < $bar.length; i++) {
-            $bar[i].style.setProperty("transform","translateX(100%)");
-            await timeout(200);
-        }
-        document.querySelector(".header-container").style.setProperty("transform","translateY(0%)");//bajamos el cursor
-        document.querySelector(".container-animation").style.setProperty("pointer-events","none");
-       
-    }
-    if(boolAtribute===1){
-        //document.querySelector(".section").style.setProperty("pointer-events","none");
-        for (let i = $bar.length-1;i>=0;i--) {
-            $bar[i].style.setProperty("transform","translateX(0%)");
-            await timeout(200);
-        }
-        if(source!==null){
-            console.log(source)
-            window.open(source,"_self","n");    
-        }
-    }     
-    return true;    
-}
-//Funcion para el modo claro y el modo oscuro
 const darkLightMode=async function($container){
     const colors={	//declaracion habitual
                 firstColor: null,
@@ -235,8 +234,6 @@ const darkLightModeStart=function(){
         $container.innerHTML=`${dinamicIcons[3]}`
     }
 }
-
-
 //Funcion para hacer la pantalla completa
 const screenChange=function(){
     const $link=document.querySelector(".screen-change");
@@ -252,6 +249,12 @@ const screenChange=function(){
     }
 }
 //FUNCION PARA SABER EN QUE SECCION ESTAMOS
+const p={ //objeto que vamos a estar utilizando tanto 
+    $section:null,
+    $caurosel:null,
+    $content:null,
+    actualPosition:null,
+}
 const scrollSpy=function(){ 
     let callback = (entries, observer) => { 
         entries.forEach((entry) => {
@@ -285,9 +288,9 @@ const scrollSpy=function(){
       });
 }
 //Funcion para mover la presentacion
-const shortcuts=function(e){
+const presentation=function(e){
 	//console.log(e);
-	console.log(e.keyCode);//tipo de evento
+	//console.log(e.keyCode);//tipo de evento
 	//los eventos del teclado como elementos de formulario
 	//e.keyCode //es el código para poder identificarla en ASCII
 	//e.key //despliega en caracteres el valor de la tecla, no es muy bueno utilizarlo porque puede variar con el dispositivo
@@ -295,14 +298,12 @@ const shortcuts=function(e){
     //e.altKey	//valores booleanos, me dice si se ha presionado
 	//e.shitfKey //valores booleanos, me dice si se ha presionado
 	//para hacer atajos con el teclado, el evento debe ser keypress
-    console.log(p)
     if(p.$caurosel.getAttribute("data-content")!="null"){// si queremos hacer el efecto carrusel
         //averiguamos en que ventana andamos actualmente
         const direction=p.$caurosel.getAttribute("data-direction"),
                         position=p.$caurosel.getAttribute("data-start");
         
         p.$content=p.$caurosel.querySelectorAll(".content");
-        console.log(p.$content);
         let total=p.$content.length;
         p.actualPosition=Number(p.$caurosel.getAttribute('data-content'));
         //hacemos el porcentaje de cuanto equivale un contenedor;
@@ -318,7 +319,6 @@ const shortcuts=function(e){
             if(p.actualPosition<total-1){//si es diferente a la ultima posicion movemos a la "derecha"
                 p.actualPosition=p.actualPosition+1;
                 console.log("Moviendose una adelante");
-               
             }
         }
         if(direction==="row"){
@@ -336,9 +336,6 @@ const shortcuts=function(e){
         if((x===0)&&(y===0)){
             p.actualPosition=0;
         }
-
-
-
         p.$caurosel.setAttribute('data-content',p.actualPosition);
         p.$caurosel.style.setProperty("transform",`translate(${x}%,${y}%)`);
     }
@@ -346,8 +343,152 @@ const shortcuts=function(e){
 }
 
 //keyup(cuando soltamos la tecla), keydown(exactamente cuando la presiono),keypress(mientras se tenga presionada la tecla)
+//Funcion para el control de jugadores
+//clase general para saber que jugador es
+class player{ 
+	constructor(playerName, numPlayer){ //si se necesitan pasar propiedades en una clase recurrimos al metodo contructor
+        this.playerName= playerName; //nombre del jugador
+		this.numPlayerESP= numPlayer; //numero de jugador segun ESP32
+        this.numPlayer=0; //numero de jugador segun la web
+        this.score=0; //puntos que lleva el jugador
+	}
+}
+const players=[null]//arreglo para ir agregando o borrando jugadors
+//prueba createNewPlayer(JSON.parse('{"numPlayer":2,"playerName":"Lalo"}'));
+//funcion para agregar un nuevo jugador a la lista
+const createNewPlayer=function(data){ //vamos a recibir un objeto JSON
+    //con el objeto JSON recibido levanto la nueva clase en una nueva posicion del arreglo
+    if(players[0]===null)players.pop();//primera vez que agregamos a jugadores
+    players.push(new player(data.playerName,data.numPlayer));
+
+    //Lo ponemos en la lista de Reglas de juego
+    const $listContainer=document.querySelector("[data-section='9'] .players"),
+            $playerContainer=document.createElement("div"),
+            $containerTxt=document.createElement("p"),
+            playersWeb=$listContainer.children.length;
+    players[players.length-1].numPlayer=playersWeb; //le asignamos el numero que va a tener en web
+    $playerContainer.setAttribute("data-num-player",playersWeb);
+    $playerContainer.setAttribute("data-click",0);
+    $playerContainer.classList.add("player-container");
+    let aux='';
+    if((playersWeb)<10)aux='0';//le agregamos un cero
+    $containerTxt.textContent=`${aux}${playersWeb}.${data.playerName}`;
+    $playerContainer.appendChild($containerTxt);
+    $listContainer.appendChild($playerContainer);
+    //Lo ponemos en la lista de loteria(donde se va a ver el score)
+}
+//prueba updatePlayer(JSON.parse('{"numPlayer":2,"playerName":"Lalopop"}'));
+const updatePlayer=function(data){ //vamos a recibir un objeto json
+    const $listContainer=document.querySelector("[data-section='9'] .players");
+    //verificamos de que jugador se trata
+    if(players[0]!=null){//osea que ya agregamos unque sea un usuario
+        players.forEach(function(el,index){ 
+            if(el.numPlayerESP===data.numPlayer){
+                //actualizamos nombre de usuario
+                const $playerContainer=$listContainer.querySelector(`[data-num-player="${el.numPlayer}"]`);
+                let aux='';
+                if(el.numPlayerESP<10)aux='0';//le agregamos un cero
+                $playerContainer.querySelector("p").textContent=`${aux}${el.numPlayer}.${data.playerName}`;
+            }
+        });
+    }
+}
+/*
+Para crear jugadores
+createNewPlayer(JSON.parse('{"numPlayer":3,"playerName":"Gerardo"}'));
+createNewPlayer(JSON.parse('{"numPlayer":6,"playerName":"Josue"}'));
+createNewPlayer(JSON.parse('{"numPlayer":1,"playerName":"Pedro"}'));
+createNewPlayer(JSON.parse('{"numPlayer":20,"playerName":"Eder Urban"}'));
+createNewPlayer(JSON.parse('{"numPlayer":21,"playerName":"Mijhal"}'));
+createNewPlayer(JSON.parse('{"numPlayer":22,"playerName":"Yohali"}'));
+createNewPlayer(JSON.parse('{"numPlayer":23,"playerName":"Natalia"}'));
+createNewPlayer(JSON.parse('{"numPlayer":24,"playerName":"Zuno"}'));
+createNewPlayer(JSON.parse('{"numPlayer":25,"playerName":"Daniela"}'));
+createNewPlayer(JSON.parse('{"numPlayer":26,"playerName":"Arian"}'));
+createNewPlayer(JSON.parse('{"numPlayer":27,"playerName":"Mario"}'));
+
+*/
+
+//funcion para borrar jugadores
+const deletePlayer=function($container=null,action,position=null){
+    console.log($container);
+    if(action===true){ //procedemos a eliminar al contenedor, le mandamos informacion al ESP32 y que le llegue 
+        let $aux,issue;
+        const $listContainer=document.querySelector("[data-section='9'] .players");
+        if($container!=null){ //implica que el que va a borrar el elemento es el boton
+            $aux=$container.parentElement;
+        }
+        else{ //se nos notifico que se desconecto un dispositivo
+            //hacer la busqueda desde aqui 
+            $aux=$listContainer.querySelector(`[data-num-player="${position}"]`);
+        }
+        const $fatherContainer=$aux,
+                numPlayer=Number($fatherContainer.getAttribute(`data-num-player`)),
+                auxArray=[];
+        $fatherContainer.remove();// quitamos el elemento html
+        players.forEach(function(el,index){ 
+            if(el.numPlayer!=numPlayer){//siempre y cuando sea 
+                auxArray.push(el);
+            }
+            else{ //es el jugador que voy a borrar, voy a enviar WEBSOCKET que se borre
+
+            }
+        });
+        players.splice(0,players.length);
+        auxArray.forEach(function(el,index){ 
+            players.push(el);
+        });
+        for (let i = 0; i < players.length; i++) {
+            let aux='';
+            if(i+1<10)aux='0';//le agregamos un cero
+            $listContainer.children[i+1].querySelector("p").textContent=`${aux}${i+1}.${players[i].playerName}`;
+            $listContainer.children[i+1].setAttribute("data-num-player",i+1);
+            players[i].numPlayer=i+1;
+        }
+    }
+    if(action===false){
+        if($container.getAttribute("data-click")==='0'){//habilitamos el boton de borrar
+            const $button=document.createElement("div");
+                $button.classList.add("delete-player-icon");
+                $button.innerHTML=`${dinamicIcons[4]}`
+                $container.setAttribute("data-click",1)
+            $container.appendChild($button);    
+        }
+        else{ //quitamos el boton de borrar
+            const $button=$container.querySelector(".delete-player-icon");
+            $button.remove();
+            $container.setAttribute("data-click",0);
+        }
+    }
 
 
+    //si si lo borramos asignar un nuevo arreglo menos esa posicion, 
+    //borramos todos los valores del original, y del que creamos ir poniendo
+    //nuevamente los valores 
+}
+//funcion en caso de que uno de los jugadores alla perdido la conexion
+
+const cardsControl=function($container){
+    
+}
+
+
+// deletePlayer(JSON.parse('{"numPlayer":2'));
+const playerConnectionLost=function(data=null,issue=null){//data es el JSON que recibimos y el issue es el asunte que queremos que envie
+    //solo recibimos que posicion era la que tenia ese jugador, encontramos el arreglo, borramos el elemento del html
+    //volvemos a asignarle numeros en la web y cambiamos textContent de p
+    if(data!=null){ //el ESP32 nos notifico que un dispositivo se desconecto
+        //buscamos el dispositivo que se desconecto y lo borramos
+
+    }
+
+
+
+}
+
+
+
+//funcion para agregar puntaje de jugadores
 
 
 //funcion para el estado de conexion de red y websocket
@@ -374,11 +515,14 @@ const conectionOnline=function(){
 }
 
 
+//funcion para enviar la carta que esta actualmente para que no puedan dar click en tarjetas que no son
+
+
 
 
 document.addEventListener("DOMContentLoaded",()=>{
     darkLightModeStart();
-    //nimationPage(null,0);
+    animationPage(null,0);
     setTimeout(()=>{
         $body.addEventListener("click",(e)=>{
             if((e.target.matches(".screen-change"))||(e.target.matches(".screen-change *"))){
@@ -390,6 +534,12 @@ document.addEventListener("DOMContentLoaded",()=>{
             if(e.target.matches(".button-container > button")){
                 animationPage(null,0);
             }
+            if(e.target.matches("[data-section='9'] .player-container")){ //lista de jugadores de reglas de juego
+                deletePlayer(e.target,false);
+            }
+            if(e.target.matches(".delete-player-icon")||e.target.matches(".delete-player-icon *")){ //lista de jugadores de reglas de juego
+                deletePlayer(e.target,true);
+            }
             
         });
     },2000); //el 1000 es tiempo en milisegundos, es el tiempo de retardo que va a ejecutar las instrucciones
@@ -399,7 +549,7 @@ document.addEventListener("DOMContentLoaded",()=>{
        // mobileNavConstructor(window.innerWidth,mobileNav[0]);  
     });
     document.addEventListener("keydown",(e)=>{
-        shortcuts(e);
+        presentation(e);
     });
     
 })
